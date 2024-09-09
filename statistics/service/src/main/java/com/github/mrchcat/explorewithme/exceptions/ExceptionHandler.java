f90fc5107c1df4802178d48314d1c9781a6ee3ef
mrchcat <m-cat@mail.ru>
2024-09-09 20:38:59 +0300

    fix: delete unnecessary dependencies

--- statistics/service/src/main/java/com/github/mrchcat/explorewithme/exceptions/ExceptionHandler.java
+++ statistics/service/src/main/java/com/github/mrchcat/explorewithme/exceptions/ExceptionHandler.java
@@ -3,23 +3,22 @@ package com.github.mrchcat.explorewithme.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 @Slf4j
 @RestControllerAdvice
-public class ExceptionHandlerClass {
+public class ExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
         log.info(ex.getMessage());
         return new ErrorResponse("Request is not valid", ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
         log.info("{}", ex.getMessage());
         return new ErrorResponse(ex.getMessage(), null);
