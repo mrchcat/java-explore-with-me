16947746d16b6447aa03a7742528214e2144ffca
mrchcat <m-cat@mail.ru>
2024-09-12 22:32:41 +0300

    refactor: make RestTemplate a bean

--- statistics/httpclient/src/main/java/com/github/mrchcat/explorewithme/StatHttpClientImpl.java
+++ statistics/httpclient/src/main/java/com/github/mrchcat/explorewithme/StatHttpClientImpl.java
@@ -7,23 +7,16 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 @Slf4j
@@ -32,10 +25,9 @@ public class StatHttpClientImpl implements StatHttpClient {
     private final String serverUrl;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd%20HH:mm:ss");
 
-    public StatHttpClientImpl(@Value("${statserver.url}") String serverUrl) {
-        this.restTemplate = new RestTemplate();
+    public StatHttpClientImpl(@Value("${statserver.url}") String serverUrl, RestTemplate restTemplate) {
         this.serverUrl = serverUrl;
-        log.info("serverUrl={}",serverUrl);
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -43,13 +35,13 @@ public class StatHttpClientImpl implements StatHttpClient {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         var request = new HttpEntity<>(createDTO, headers);
-            URI url = UriComponentsBuilder
-                    .fromUriString(serverUrl)
-                    .path("/hit")
-                    .build()
-                    .toUri();
+        URI url = UriComponentsBuilder
+                .fromUriString(serverUrl)
+                .path("/hit")
+                .build()
+                .toUri();
         try {
-            restTemplate.exchange(url, HttpMethod.POST,request,Object.class);
+            restTemplate.exchange(url, HttpMethod.POST, request, Object.class);
         } catch (Exception e) {
             log.error("Statistical service can not add data {}. Stacktrace: {}",
                     createDTO, e.getStackTrace());
@@ -72,13 +64,11 @@ public class StatHttpClientImpl implements StatHttpClient {
         ParameterizedTypeReference<List<RequestStatisticDto>> ptr = new ParameterizedTypeReference<>() {
         };
         try {
-            var responseEntity =restTemplate.exchange(url, HttpMethod.GET,null,ptr);
+            var responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, ptr);
             return responseEntity.getBody();
         } catch (Exception e) {
-            log.error("""
-                    Statistical service did not answer for get request with parameters {}.
-                    Stacktrace: {}
-                    """, queryParams, e.getStackTrace());
+            log.error("Statistical service did not answer for get request with parameters {}.Stacktrace: {}",
+                    queryParams, e.getStackTrace());
             throw new IOException(e.getMessage());
         }
     }
